e796ea29d1e0d0673f59b3c986f2af1d98c0d9d3
Devin Hale <101072122+devin-hale@users.noreply.github.com>
2023-11-27 11:22:45 -0600

    Update treesitter.lua

--- packages/neovim/lua/aura-theme/groups/treesitter.lua
+++ packages/neovim/lua/aura-theme/groups/treesitter.lua
@@ -75,8 +75,20 @@ local function createTreeSitterGroup(palette, aura)
     ["@variable"] = {fg = palette.white},
 
     --LSP Semantic Token Groups
-    ["@lsp.type.variable"] = {fg = palette.white},
-    ["@lsp.type.interface"] = aura.Blue,
+    ['@lsp.type.namespace'] = '@namespace',
+    ['@lsp.type.type'] = '@type',
+    ['@lsp.type.class'] = '@type',
+    ['@lsp.type.enum'] = '@type',
+    ['@lsp.type.interface'] = '@type',
+    ['@lsp.type.struct'] = '@structure',
+    ['@lsp.type.parameter'] = '@parameter',
+    ['@lsp.type.variable'] = '@variable',
+    ['@lsp.type.property'] = '@property',
+    ['@lsp.type.enumMember'] = '@constant',
+    ['@lsp.type.function'] = '@function',
+    ['@lsp.type.method'] = '@method',
+    ['@lsp.type.macro'] = '@macro',
+    ['@lsp.type.decorator'] = '@function',
     
 
   }
